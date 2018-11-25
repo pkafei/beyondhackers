abf978aedc85afa2b03c8e5fd4e904f1af8c977a
Portia Burton <plburton@gmail.com>
2018-11-25 17:35:24 +0100

    Hard coding about me info in the sidebar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "Distributed tech and IOT Apps",
+    author: "Portia Burton",
+    description: "coder/thinker/provocateur"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'red',
+        theme_color: 'red',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
