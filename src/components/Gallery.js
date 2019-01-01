9c152a63c42385fd08434de6d667c221740c44ed
Portia Burton <plburton@gmail.com>
2019-01-01 16:32:08 +0100

    Deleted more of legacy gallery logic

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,41 +10,7 @@ class Gallery extends Component {
             lightboxIsOpen: false,
             currentImage: 0,
         };
-
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.gotoImage = this.gotoImage.bind(this);
         this.handleClickImage = this.handleClickImage.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-    }
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    gotoImage (index) {
-        this.setState({
-            currentImage: index,
-        });
     }
     handleClickImage () {
         if (this.state.currentImage === this.props.images.length - 1) return;
@@ -66,7 +32,6 @@ class Gallery extends Component {
                     >
                         <img src={obj.thumbnail} />
                     </a>
-
                     <h3>{obj.caption}</h3>
                     <p>{obj.description}</p>
                 </article>
