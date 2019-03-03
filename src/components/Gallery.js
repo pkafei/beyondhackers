ace7b0189697b1359dbbc0119a01b39b3ac24ec9
Portia Burton <plburton@gmail.com>
2019-03-03 14:13:21 +0100

    Made the images slightly bigger

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -28,7 +28,7 @@ class Gallery extends Component {
                 <div className="fl w-100 w-50-ns pa2">
                     <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
                         <div className="tc">
-                            <img src={obj.thumbnail} className="br-100 h3 w3 dib" title="Photo of a kitty staring at you" />
+                            <img src={obj.thumbnail} className="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" />
                             <h1 className="f4">{obj.caption}</h1>
                             <hr className="mw3 bb bw1 b--black-10" />
                         </div>
