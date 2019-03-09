66de7de38277a7c4beaa6b9932a91708c7596be6
Portia Burton <plburton@gmail.com>
2019-03-09 15:36:41 +0100

    Added Tech Portfolio Hero website

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,14 @@ import Gallery from '../components/Gallery'
 
 import thumb01 from '../assets/images/thumbs/beyond_hackers_bicycle_collective.png'
 import thumb02 from '../assets/images/thumbs/beyond_hackers_distributed_health_care.png'
+import thumb03 from '../assets/images/thumbs/tech_portfolio_hero.png'
 
 const DEFAULT_IMAGES = [
     { id: '1', src: 'https://pricey-lancer.glitch.me/', thumbnail: thumb01, caption: 'Bicycles on the Blockchain', description: 'Rent bikes on the blockchain.'},
     { id: '2', src: 'https://rowan-pilot.glitch.me/', thumbnail: thumb02, caption: 'Distributed Health Care Identity', description: 'Decentralized sign-in example.'},
+    { id: '3', src: 'https://www.techportfoliohero.com/', thumbnail: thumb03, caption: 'Tech Portfolio Hero', description: 'Career consulting for developers.'},
+
+
 ];
 
 class HomeIndex extends React.Component {
